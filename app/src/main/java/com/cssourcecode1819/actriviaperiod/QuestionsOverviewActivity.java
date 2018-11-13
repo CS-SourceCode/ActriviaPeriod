8e39f5ba2d27ee8ad8f19faef20c0bc8d8bcb7af
Thaumaturgy <deweysia@gmail.com>
2018-11-13 13:19:24 +0800

    Add necessary activities and layouts

---
+++ app/src/main/java/com/cssourcecode1819/actriviaperiod/QuestionsOverviewActivity.java
@@ -0,0 +1,13 @@
+package com.cssourcecode1819.actriviaperiod;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuestionsOverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions_overview);
+    }
+}
