670e4c52065489829bf4a5ff1d446115be7af709
Thaumaturgy <deweysia@gmail.com>
2018-11-13 14:58:43 +0800

    Add basic navigation for Login and Welcome screens

--- app/src/main/java/com/cssourcecode1819/actriviaperiod/LoginActivity.java
+++ app/src/main/java/com/cssourcecode1819/actriviaperiod/LoginActivity.java
@@ -1,13 +1,48 @@
 package com.cssourcecode1819.actriviaperiod;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    private View.OnClickListener onClickListenerSignIn = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            EditText etTeamName = findViewById(R.id.activity_login_etTeamID);
+            EditText etPassword = findViewById(R.id.activity_login_etPassword);
+
+            String teamName = etTeamName.getText().toString();
+            String password = etPassword.getText().toString();
+
+            boolean successful = signIn(teamName, password);
+
+            if (successful){
+                Intent i = new Intent(LoginActivity.this, WelcomeActivity.class);
+                startActivity(i);
+                LoginActivity.this.finish();
+            }
+            else
+                Toast.makeText(LoginActivity.this, "Could not login successfully", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private boolean signIn(String username, String password){
+        return true;
+        // Todo: Save login details to preferences
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btnSignIn = findViewById(R.id.activity_login_btnSignIn);
+        btnSignIn.setOnClickListener(onClickListenerSignIn);
     }
 }
--- app/src/main/java/com/cssourcecode1819/actriviaperiod/WelcomeActivity.java
+++ app/src/main/java/com/cssourcecode1819/actriviaperiod/WelcomeActivity.java
@@ -1,13 +1,46 @@
 package com.cssourcecode1819.actriviaperiod;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private View.OnClickListener onClickListenerProceed = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            EditText etTeamName = findViewById(R.id.activity_welcome_etTeamName);
+
+            String teamName = etTeamName.getText().toString();
+            boolean successful = setUpTeamName(teamName);
+
+            if (successful){
+                Intent i = new Intent(WelcomeActivity.this, QuestionsOverviewActivity.class);
+                startActivity(i);
+                WelcomeActivity.this.finish();
+            }
+            else
+                Toast.makeText(WelcomeActivity.this, "Could not set team name", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private boolean setUpTeamName(String teamName){
+        // Todo: Wire up team name to account and shared preferences
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button btnProceed = findViewById(R.id.activity_welcome_btnProceed);
+        btnProceed.setOnClickListener(onClickListenerProceed);
     }
+
+
 }
